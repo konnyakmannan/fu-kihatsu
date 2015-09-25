29552bebc48b5eb5048e5c203a35d26ff119620b
Tomohide Takao <dorayaki.tomo@gmail.com>
2015-09-25 21:08:20 +0900

    It works.

--- main.js
+++ main.js
@@ -1,58 +1,24 @@
-'use strict';
-
 var app = require('app');
-var BroserWindow = require('browser-window');
-var Menu = require('menu');
+var BrowserWindow = require('browser-window');
 
 require('crash-reporter').start();
 
 var mainWindow = null;
 
-app.on('window-all-closed', function() {
-  if (precess.platform !== 'darwin')
-    app.quit();
-});
+app.on('window-all-closed', function() { app.quit(); });
 
 app.on('ready', function() {
-  mainWindow = new BroserWindow({ width: 800, height: 600});
+  mainWindow = new BrowserWindow({
+                    width: 800,
+                    height: 600
+                  });
+
   mainWindow.loadUrl('file://' +　__dirname + '/index.html');
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
+  mainWindow.on('closed', function() { mainWindow = null; });
+
+  mainWindow.on('will-navigate', function(e, url) {
+    e.preventDefault();
+    openExternal(url);
   });
 });
-
-var template = [
-  {
-    label: 'ReadUs',
-    submenu: [
-      {label: 'Quit', accelerator: 'Command+Q', click: function() { app.quit(); }}
-    ]
-  },
-  {
-    label: 'File',
-    submenu: [
-      {label: 'Open', accelerator: 'Command+O', click: function() {
-        require('dialog').showOpenDialog({properties: ['openDirectory']},
-                            function(baseDir) {
-                              if (baseDir && baseDir[0]) {
-                                openWindow(baseDir[0]);
-                              }
-                            });
-      }}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {label: 'Reload', accelerator: 'Command+R', click: function() {
-        BroserWindow.getFocusedWindow().reloadIgnoringCache();
-      }},
-      {label: 'Toggle DevTools', accelerator: 'Alt+Command+I', click: function() {
-        BroserWindow.getFocusedWindow.toggleDevTools();
-      }}
-    ]
-  }
-];
-
-var menu = Menu.buildFromTemplate(template);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,119 @@
+import fs from 'fs';
 import remote from 'remote';
-// const fileUtil = remote.require('./build/lib/fileUtil');
-// 
-// fileUtil.fetchReadmeList((err, matches) => {
-//   if (!err) document.write(matches.join());
-// });
-const editor = ace.edit('input_text');
-editor.getSession().setMode('ace/mode/tex');
-editor.setTheme('ace/theme/twilight');
+
+const dialog = remote.require('dialog');
+const browserWindow = remote.require('browser-window');
+
+var currentPath = '';
+
+window.onload = () => {
+  const editor = ace.edit('input_text');
+  editor.getSession().setMode('ace/mode/markdown');
+  editor.setTheme('ace/theme/twilight');
+
+  editor.on('change', () => {
+    const view = document.getElementById('view_text');
+
+    const txt = editor.getValue();
+    const convertedText = txt.replace(/(?:\r\n|\r|\n)/g, '<br />');
+    view.innerHTML = convertedText;
+
+    MathJax.Hub.Configured();
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, view])
+  });
+
+  var btnOpen = document.getElementById('btnOpen');
+  btnOpen.addEventListener('click', () => { openLoadFile(editor); });
+
+  var btnSave = document.getElementById('btnSave');
+  btnSave.addEventListener('click', () => { saveFile(editor); });
+};
+
+function openLoadFile(editor) {
+  const win = browserWindow.getFocusedWindow();
+  dialog.showOpenDialog(
+    win,
+    {
+      properties: ['openFile'],
+      filters: [
+        {
+          name: 'Documents',
+          extensions: ['md']
+        }
+      ]
+    },
+    (path) => {
+      if (path) {
+        readFile(editor, path[0]);
+      }
+    }
+  );
+}
+
+function readFile(editor, path) {
+  currentPath = path;
+  fs.readFile(path, (e, text) => {
+    if (e !== null) {
+      alert('error: ' + e);
+      return;
+    }
+    const footer = document.getElementById('footer_fixed');
+    footer.innerHTML = path;
+    editor.setValue(text.toString(), -1);
+  });
+}
+
+function saveFile(editor) {
+  const win = browserWindow.getFocusedWindow();
+  if (currentPath === '') {
+    saveNewFile(editor);
+    return;
+  }
+  dialog.showMessageBox(
+    win,
+    {
+      title: 'ファイルの上書き保存を行います',
+      type: 'info',
+      buttons: ['OK', 'Cancel'],
+      detail: '本当に保存しますか?'
+    },
+    (response) => {
+      if (response === 0) {
+        const data = editor.getValue();
+        writeFile(currentPath, data);
+      }
+    }
+  );
+}
+
+function saveNewFile(editor) {
+  const win = browserWindow.getFocusedWindow();
+  dialog.showOpenDialog(
+    win,
+    {
+      properties: ['openFile'],
+      filters: [
+        {
+          name: 'Documents',
+          extensions: ['md']
+        }
+      ]
+    },
+    (path) => {
+      if (path) {
+        const data = editor.getValue();
+        currentPath = path;
+        writeFile(currentPath, data);
+      }
+    }
+  );
+}
+
+function writeFile(path, data) {
+  fs.writeFile(path, data, (e) => {
+    if (e !== null) {
+      alert('error: ' + e);
+      return;
+    }
+  });
+}
