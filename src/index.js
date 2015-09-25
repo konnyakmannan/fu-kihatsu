cf1357d80d68d918fdb41045149268d2707035a7
KANDA Taiki <konnyakmannan@gmail.com>
2015-09-25 14:29:20 +0900

    Write the base of the editor.
    - Editor
    - Viewer
    - Save/Open buttom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import remote from 'remote';
-const fileUtil = remote.require('./build/lib/fileUtil');
-
-fileUtil.fetchReadmeList((err, matches) => {
-  if (!err) document.write(matches.join());
-});
+// const fileUtil = remote.require('./build/lib/fileUtil');
+// 
+// fileUtil.fetchReadmeList((err, matches) => {
+//   if (!err) document.write(matches.join());
+// });
+const editor = ace.edit('input_text');
+editor.getSession().setMode('ace/mode/tex');
+editor.setTheme('ace/theme/twilight');
